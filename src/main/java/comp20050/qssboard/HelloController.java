05612f81e56fb46f76ee433df3aa4833cc3dc57b
Andres Lauder <camodres@gmail.com>
2026-02-20 15:44:25 +0000

    added title and working on centering the board

--- src/main/java/comp20050/qssboard/HelloController.java
+++ src/main/java/comp20050/qssboard/HelloController.java
@@ -4,29 +4,24 @@
 
 package comp20050.qssboard;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Polygon;
-import kotlin.NotImplementedError;
-import javafx.scene.paint.Color;
 
 public class HelloController {
-    // Functions from backend - logic for handling the player
-    GameState state = new GameState();
-
-    // colours of different players
-    Color colorP1 = Color.WHITE;
-    Color colorP2 = Color.BLACK;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
+
+    @FXML // fx:id="BlackBorders"
+    private Group BlackBorders; // Value injected by FXMLLoader
 
     @FXML // fx:id="BottomLetters"
     private Group BottomLetters; // Value injected by FXMLLoader
@@ -700,53 +695,34 @@ public class HelloController {
     @FXML // fx:id="RightNumbers"
     private Group RightNumbers; // Value injected by FXMLLoader
 
+    @FXML // fx:id="ShapeLayout"
+    private Group ShapeLayout; // Value injected by FXMLLoader
+
+    @FXML // fx:id="Title"
+    private Group Title; // Value injected by FXMLLoader
+
     @FXML // fx:id="TopLetters"
     private Group TopLetters; // Value injected by FXMLLoader
 
+    @FXML // fx:id="boardWrapper"
+    private Group boardWrapper; // Value injected by FXMLLoader
+
+    @FXML // fx:id="root"
+    private StackPane root; // Value injected by FXMLLoader
+
     @FXML
     void getCellID(MouseEvent event) {
-        Polygon cell = (Polygon) event.getSource();
 
-        // get row and col
-        Position pos = new Position(cell.getId());
-        QuaxBoard.TileType tile_type;
-
-        // get tile type:
-        if (cell.getId().charAt(0) == 'R'){
-            tile_type = QuaxBoard.TileType.RHOMBUS;
-        }
-        else {
-            tile_type = QuaxBoard.TileType.OCTAGON;
-        }
-
-        // make the move:
-        GameState.Player playerBeforeMove = state.getCurrentPlayer();
-        boolean success = state.makeMove(pos, tile_type);
-        if (!success) {
-            System.out.println("print");
-            // add some text here on teh screen
-            // TO-DO
-            throw new NotImplementedError();
-        }
-
-        if (playerBeforeMove == GameState.Player.P1) {
-            cell.setFill(colorP1);
-        }
-        else if (playerBeforeMove == GameState.Player.P2) {
-            cell.setFill(colorP2);
-        }
-        else { // player must be either P1 or P2. Otherwise, we have an error
-            throw new IllegalArgumentException("Error from Controller in getCellID - NO player option given");
-        }
     }
 
     @FXML
     void getHexID(MouseEvent event) {
-        getCellID(event);
+
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert BlackBorders != null : "fx:id=\"BlackBorders\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert BottomLetters != null : "fx:id=\"BottomLetters\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert LeftNumbers != null : "fx:id=\"LeftNumbers\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert OctCell_r0_c0 != null : "fx:id=\"OctCell_r0_c0\" was not injected: check your FXML file 'hello-view.fxml'.";
@@ -971,7 +947,11 @@ public class HelloController {
         assert Rhombus_r9_c8 != null : "fx:id=\"Rhombus_r9_c8\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert Rhombus_r9_c9 != null : "fx:id=\"Rhombus_r9_c9\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert RightNumbers != null : "fx:id=\"RightNumbers\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert ShapeLayout != null : "fx:id=\"ShapeLayout\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert Title != null : "fx:id=\"Title\" was not injected: check your FXML file 'hello-view.fxml'.";
         assert TopLetters != null : "fx:id=\"TopLetters\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert boardWrapper != null : "fx:id=\"boardWrapper\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert root != null : "fx:id=\"root\" was not injected: check your FXML file 'hello-view.fxml'.";
 
     }
 
